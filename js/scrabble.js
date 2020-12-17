5f2bbfef5d8dfce133f7b930f0095d8e37067384
justrollinc <bandgamer123@yahoo.com>
2020-12-16 22:38:36 -0500

    Added scrabble update

--- js/scrabble.js
+++ js/scrabble.js
@@ -1,3 +1,12 @@
+/*
+Cameron Napolitano
+cameron_naplitano@student.uml.edu
+UMass Lowell Student
+91.61 GUI Progamming
+12/16/2020
+This file defines the scripts used by scrabble.html.
+*/
+
 /* A simple implementation of the pieces, but easy to understand while accurately recreating the probability involved */
 var pieces = [     
     'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
@@ -58,14 +67,33 @@ var PieceCosts = {
     'Blank': 0
 }
 
+// NOTE: not certain about the scope of ready
+// Copy and shuffle pieces
+var workingPieces = [...pieces];
+shuffle(workingPieces);
+
+// Dictionary of all pieces
+var holder_pieces = {};
+
+// Current Word & Score
+var current_word = ['_', '_', '_', '_', '_', '_', '_'];
+var current_score = 0;
+
+// IDs of Letters on board so we know which ones to remove
+var board_letter_IDs = [];
+
 // Generic Tile Class
 class Tile {
-    static currentID = 0;
+    static currentID = 7;
     static imgNameFormat = '../graphics_data/tiles/Scrabble_Tile_?.jpg'
 
     constructor(type) {
         this.type = type;
-        this.ID = 'tile' + Tile.currentID++;
+        this.isBlank = (type === 'Blank');
+        this.ID = Tile.currentID++;
+        this.location = 'holder';
+        
+        // Construct html
         var html = [
             '<div id=', this.ID,' class="col-2">',
                 '<img class="tile" src="', Tile.imgNameFormat.replace('?', type), '" alt="TILE">',
@@ -101,19 +129,179 @@ function shuffle(array) {
     return array;
 }
 
+// Populates board with numberOfPieces as the number of pieces to add.
+function populateBoard(numberOfPieces) {
+    for (var i = 0; i < numberOfPieces && workingPieces.length > 0; i++) {
+        var piece = new Tile(workingPieces.pop());
+        holder_pieces[piece.ID] = piece;
+    }
+    $('#remain').text('pieces remaing: ' + workingPieces.length);
+}
 $(document).ready(function() {
-    
-    $('.board').droppable();
-    $('#holder').droppable();
+    // Define board behavior
+    $('.board').droppable({drop: function(event, ui) {
+            // Get IDs
+            var piece_ID = ui.draggable.attr('id');
+            var board_ID = $(this).attr('id');
 
-    // Copy and shuffle pieces
-    var workingPieces = pieces;
-    workingPieces = shuffle(workingPieces);
+            // Check if the tile moved
+            var tile = holder_pieces[piece_ID];
+            if (tile.location === board_ID) {
+                // Nothing has changed
+                return;
+            }
+
+            // Update old location
+            if (tile.location != 'holder') {
+                current_word[tile.location] = '_';
+                $('#word').text('current word:' + current_word.join(''));
+
+            }
+
+            // Prompt user to set blank value
+            // if (tile.isBlank) {
+            //     var dialog = [
+            //         '<div id="dialog', piece_ID,'" title="Select Letter">',
+            //             '<input id="input', piece_ID, '" type="text" pattern="[A-Z]">',
+            //             '<button id="button', piece_ID, '" type="button" class="btn btn-secondary col">Submit</button>',
+            //         '</div>'].join('');
+            //     $('body').append(dialog);
+            //     $('#dialog' + piece_ID).dialog();
+            //     $('#button' + piece_ID).click(function() {
+            //         // get value
+            //         var id = $(this).attr('id').splice(-1);
+            //         window.alert(id);
+            //         var val = $('#button' + id).val();
+                    
+            //         // Update tile
+            //         $('#' + id).find('img').attr('src', Tile.imgNameFormat.replace('?', val));
+            //         holder_pieces[id].type = val;
+
+            //         // Remove this block
+            //         $('.ui-dialog').remove();
+            //     });
+
+            // }
+            tile.location = board_ID;
+            // Get and assign letter
+            var new_letter = tile.type;
+            current_word[board_ID] = new_letter;
+            $('#word').text('current word:' + current_word.join(''));
+
+            // Add to used pieces list
+            board_letter_IDs.push(piece_ID);
+        }
+    });
+    $('#holder').droppable({drop: function(event, ui) {
+        // Update tile location
+        var piece_ID = ui.draggable.attr('id');
+        var tile = holder_pieces[piece_ID];
+        var board_ID = tile.location;
+        if (board_ID != 'holder') {
+            holder_pieces[piece_ID].location = 'holder';
+
+            //  Update word
+            current_word[board_ID] = '_';
+            $('#word').text('current word: ' + current_word.join(''));
+
+            // Remove from board letters       
+            var index = board_letter_IDs.indexOf(piece_ID);
+            if (index != -1) {
+                board_letter_IDs.splice(index, 1);
+            }
+        }
+    }});
 
     // Initialize Board
-    var holder_pieces = [];
-    for (var i = 0; i < 7; i++) {
-        var piece = new Tile(workingPieces.pop());
-        holder_pieces.push(piece);
-    }
+    populateBoard(7);
+
+    // Initialize Buttons
+    $('#submit').click(function() {
+        // Verify and Get Word
+        var i;
+        var word = '';
+        var score = 0;
+        for (i = 0; i < 6; i++) {
+            var letter = current_word[i];
+            if (letter === '_')
+                break;
+
+            // Add letter and compute
+            word += letter;
+            var multiplier = (i === 1 || i === 5) ? 2 : 1;
+            var tile_cost = PieceCosts[letter];
+            score += multiplier * tile_cost;
+        }
+
+        // Look for any left over characters
+        for (++i; i < 6; i++) {
+            if (current_word[i] != '_') {
+                // NOTE: This should display some error, but for now we will return
+                return
+            }
+        }
+
+        if (word.length === 0) {
+            // NOTE: Should also error here
+            return
+        }
+
+        // Update html
+        current_score += score;
+        $('#score').text('score: ' + current_score.toString());
+        current_word = ['_', '_', '_', '_', '_', '_', '_'];
+        $('#word').text('current word:' + current_word.join(''));
+
+        // Remove used letters from play
+        for (var j = 0; j < board_letter_IDs.length; j++) {
+            var id = board_letter_IDs[j];
+            $('#' + id).remove();
+            delete holder_pieces[id];
+        }
+
+        // Get new letters
+        populateBoard(board_letter_IDs.length);
+
+        // Reset
+        board_letter_IDs = [];
+    });
+    $('#reset').click(function() {
+        // Reset the current round
+        current_word = ['_', '_', '_', '_', '_', '_', '_'];
+        $('#word').text('current word:' + current_word.join(''));
+
+        // Remove pieces and return to sack
+        for (id in holder_pieces) {
+            $('#' + id).remove();
+            workingPieces.push(holder_pieces[id].type);
+            shuffle(workingPieces);
+        }
+        
+        // Reset state variables
+        holder_pieces = {};
+        board_letter_IDs = [];
+
+        // Re-populate rack
+        populateBoard(7);
+        
+    });
+    $('#restart').click(function() {
+        // Start a new game
+        current_word = ['_', '_', '_', '_', '_', '_', '_'];
+        $('#word').text('current word:' + current_word.join(''));
+        current_score = 0;
+        $('#score').text('score: ' + current_score);
+
+        // Reset state variables
+        holder_pieces = {};
+        board_letter_IDs = [];
+
+        // Copy and shuffle
+        workingPieces = [...pieces];
+        shuffle(workingPieces);
+
+        // Remove all tiles
+        $('#holder').empty();
+        populateBoard(7);
+    });
 });
